e50f9e5cf4367a6ea42b249fbb96826dcea5f6a3
DevAdedeji <adedejitewogbade2@gmail.com>
2025-08-18 10:26:23 +0100

    Implemented logout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,25 @@
+import { useAuthStore } from "@/store/useAuthStore";
+import { useEffect, useState } from "react";
+import Button from "./ui/Button";
+import { signOut, getAuth } from "firebase/auth";
+import toast from "react-hot-toast";
 const Header = () => {
+    const { user } = useAuthStore();
+    const [isLoading, setIsLoading] = useState(false);
+    const auth = getAuth();
+    useEffect(() => {
+        console.log(user)
+    }, [user])
+    const logUserOut = async () => {
+        try {
+            setIsLoading(true);
+            await signOut(auth)
+        } catch (e: any) {
+            toast.error(e.message || "An unknown error occurred.");
+        } finally {
+            setIsLoading(false);
+        }
+    }
     return (
         <header className="flex items-center justify-center min-h-[60px]">
             <nav className="w-[90%] mx-auto flex items-center justify-between py-4">
@@ -6,6 +27,10 @@ const Header = () => {
                     <img src="/images/logo.png" className="size-[60px] object-cover" />
                     <p className="text-charcoal uppercase font-bold">FRINNA</p>
                 </div>
+                {
+                    user &&
+                    <Button onClick={logUserOut} disabled={isLoading}>Log out</Button>
+                }
             </nav>
         </header>
     )
