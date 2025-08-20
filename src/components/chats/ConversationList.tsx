f2bbb86c3fb75250fab97e7139151ece57b0f742
DevAdedeji <adedejitewogbade2@gmail.com>
2025-08-20 20:45:19 +0100

    Want to test if the github action is working

--- src/components/chats/ConversationList.tsx
+++ src/components/chats/ConversationList.tsx
@@ -8,7 +8,7 @@ interface ConversationListProps {
 }
 
 const ConversationList = ({ onConversationSelect, selectedConversationId }: ConversationListProps) => {
-    const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, `12`, 13, 14, 15, 16, 17, 18, 19, 20];
+    const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
     const { user } = useAuthStore();
     return (
         <div className="w-full flex flex-col gap-4 bg-background max-h-[90vh] py-4 sm:py-10 px-4 sm:px-9 overflow-y-hidden">
