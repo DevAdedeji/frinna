88cc3174132ac21ab25da7a71d5863cdc60c5223
DevAdedeji <adedejitewogbade2@gmail.com>
2025-08-21 23:32:09 +0100

    Fixed app text description

--- src/layouts/auth.tsx
+++ src/layouts/auth.tsx
@@ -9,7 +9,7 @@ const AuthLayout = () => {
                 <div className="flex flex-col lg:flex-row gap-10 lg:gap-0 items-center justify-between">
                     <div className="lg:order-1 order-2 flex flex-col gap-2 sm:gap-8 w-full sm:w-[500px]">
                         <img src="/images/mockup.png" className="sm:h-[600px] w-full object-contain" alt="mockup" />
-                        <p className="text-base text-center">Frinna.xyz is a free-to-use anonymous messaging website where you can register, login, share your link, get messages and continue chatting with your anonymous friend.</p>
+                        <p className="text-base text-center">Frinna is a free-to-use anonymous messaging website where you can register, login, share your link, get messages and continue chatting with your friends</p>
                     </div>
                     <div className="w-full sm:w-[500px] lg:order-2 order-1">
                         <Outlet />
